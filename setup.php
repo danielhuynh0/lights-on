6527d158e4ceb43ddef7aca81e3f6c8ffa8526e9
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-20 17:27:49 -0500

    return json objects for randomized lights on locations

--- setup.php
+++ setup.php
@@ -9,13 +9,15 @@ if (isset($_GET)) {
     if(isset($_GET["rowsInput"])) {
         $rows = $_GET["rowsInput"];
     } else {
-        // error message
+        header("Location: index.html");
+        exit();
     }
 
     if(isset($_GET["colsInput"])) {
         $columns = $_GET["colsInput"];
     } else {
-        // error message
+        header("Location: index.html");
+        exit();
     }
 
     if($rows * $columns < 7) {
@@ -24,9 +26,31 @@ if (isset($_GET)) {
                 $board_array[$i][$j] = 1; // 1 = on, 0 = off
             }
         }
-        echo json_encode($board_array);
-    } else {
 
+        echo json_encode($board_array);
+
+    } else {
+        $lights_on_array = array();
+        while(count($lights_on_array) < 7) {
+            $row = rand(0, $rows - 1);
+            $col = rand(0, $columns - 1);
+            if(!in_array(array($row, $col), $lights_on_array)) {
+                array_push($lights_on_array, array($row, $col));
+            }
+        }
+
+        for($i = 0; $i < $rows; $i++) {
+            for($j = 0; $j < $columns; $j++) {
+                $board_array[$i][$j] = 0; // 1 = on, 0 = off
+            }
+        }
+
+        for($i = 0; $i < count($lights_on_array); $i++) {
+            $board_array[$lights_on_array[$i][0]][$lights_on_array[$i][1]] = 1;
+        }
+
+        echo json_encode($board_array);
+        
     }
 } else {
     header("Location: index.html");
