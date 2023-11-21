eb9b43ac38e761947fbbec453c0edbfbcf7d5467
Eric Li <lieric021@gmail.com>
2023-11-21 04:24:42 -0500

    finished

--- game_controller.js
+++ game_controller.js
@@ -1,4 +1,5 @@
 var state=[];
+var won = false;
 
 $(document).ready(function() {
     $("#submit").click(function(event) {
@@ -17,6 +18,7 @@ $(document).ready(function() {
             success: function(result) {
                 var resultData=JSON.parse(result)
                 state=resultData;
+                won=false;
                 generateTable(rows, cols, resultData);
             },
             error: function(jqXHR, textStatus, errorThrown) {
@@ -45,27 +47,80 @@ function generateTable(rows, cols, data){
             cell.on("mouseout", function() {
                 $(this).removeClass("hover_box");
             });
+            cell.on("click", function() {
+                var index = $(this).data('value');
+                updateState(rows, cols, index);
+                checkIfWin(rows, cols);
+            });
             count++;
         }
         $("#board").append("</tr>");
     }
 }
 
-function checkIfWin() {
+function updateState(rows, cols, index){
+    if(won){
+        return;
+    }
+    var y = Math.floor(index / cols);
+    var x = index-(y*cols);
+    flip(x, y);
+    if(x-1>=0){
+        flip(x-1, y);
+    }
+    if(x+1<cols){
+        flip(x+1, y);
+    }
+    if(y-1>=0){
+        flip(x, y-1);
+    }
+    if(y+1<rows){
+        flip(x, y+1);
+    }
+    updateTable(cols);
+}
 
+function flip(x, y){
+    if(state[y][x]==1){
+        state[y][x]=0;
+    }
+    else{
+        state[y][x]=1;
+    }
+}
+
+function updateTable(number){
+    var count = 0;
+    $('#board').each(function (rowIndex, row) {
+        $(row).find('td').each(function (colIndex, col) {
+            var y = Math.floor(count / number);
+            var x = count-(y*number);
+            count = count+1;
+            if(state[y][x]==1){
+                $(col).removeClass("off_box");
+                $(col).addClass("on_box");
+            }
+            else{
+                $(col).removeClass("on_box");
+                $(col).addClass("off_box");
+            }
+        });
+    });
+}
+
+function checkIfWin(rows, cols) {
     let win = true;
 
-    for(let i=0; i<state.length; i++) {
-        if(state[i] == 1) {
-            win = false;
-            break;
-        }
-        else {
-            win = true;
+    for(var y=0; y<rows; y++){
+        for(var x=0; x<cols; x++) {
+            if(state[y][x] == 1) {
+                win = false;
+            }
         }
     }
 
     if(win) {
+        won=true;
         displayWinMessage();
     }
 }
