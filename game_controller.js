f1b59bdc3266e68ba059ce65ab4c200d24e47d75
Eric Li <lieric021@gmail.com>
2023-11-21 01:28:56 -0500

    added state

--- game_controller.js
+++ game_controller.js
@@ -1,4 +1,4 @@
-
+var state=array();
 $(document).ready(function() {
     generateTable(5, 6, null);
     $("#submit").click(function(event) {
@@ -15,9 +15,6 @@ $(document).ready(function() {
             type: "GET",
             data: {rowsInput: rows, colsInput: cols},
             success: function(result) {
-                // $("#output").html(data);
-                // Eric: edit this to make the game board appear
-                // display the data that you want.
                 $("#errorMessage").text("result");
                 generateTable(rows, cols, data);
             },
@@ -31,6 +28,7 @@ $(document).ready(function() {
 });
 
 function generateTable(rows, cols, data){
+    $("#board").empty();
     alert(rows);
     for(var y=0; y<rows; y++){
         $("#board").append("<tr>");
